be90650f0733244d4c9432104236fd504445061b
vs37nx <vs37nx@gmail.com>
2014-05-19 17:58:41 +1000

    Im Back Baby!!!
    After a long hiatus (School) I decided to brush the dust off the
    LegitJIT project.
    Added Makefile + Code prettification

--- legitJIT/src/main.c
+++ legitJIT/src/main.c
@@ -14,14 +14,11 @@
 
 #include "memManager.h"       /** MemoryManagement **/
 #include "asmInstructions.h"  /** The actual logic **/
-#include "timer.h"
-
-#define debug
 
 struct memManagerVars mMV;
 
-void setupData(void){
-
+void setupData(void)
+{
 	movEaxNum(0); /** padding for awnser **/
 	imul(5, 7);
 	popEax();
@@ -35,67 +32,18 @@ void setupData(void){
 
 }
 
-int stdMul(void){
-	int x,y;
-
-#ifdef debug
-	startTimer();
-#endif
-
-	x = 5;
-	y = 7;
-
-	return x*y;
-}
-
 /** int argc, char *argv[] **/
 int main()
 {	
 	long res;
-	
-#ifdef debug
-	startTimer();
-#endif
 
 	setupData();
-	
-#ifdef debug
-	stopTimer();
-	reportTime("setupData", time_elapsed_nanos);
-	time_elapsed_nanos = 0.0;
-	startTimer();
-#endif
-	
 	res = executeMem();
-	
-#ifdef debug
-	stopTimer();
-	reportTime("imul", time_elapsed_nanos);
-#endif
 	
 	printf("JIT imul result = %d\n", res);
 
 	res = 0;
-
 	freeMem();
-
-/*****************************************************************************************************/
-/*****************************************************************************************************/
-/*****************************************************************************************************/
-
-#ifdef debug
-	startTimer();
-#endif
-	
-	res = stdMul();
-
-#ifdef debug
-	stopTimer();
-	reportTime("std c imul", time_elapsed_nanos);
-#endif
-	
-	printf("std c imul result = %ld\n", res);
-	res = 0;
 
 	return 0;
 }
